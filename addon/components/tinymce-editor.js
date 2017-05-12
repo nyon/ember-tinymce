f0fba0a791e1aeec8b7af02dbd69e17cd93427b9
nyon <me@nyon.de>
2017-05-12 17:06:19 +0200

    pass arguments to loadScripts properly

--- addon/components/tinymce-editor.js
+++ addon/components/tinymce-editor.js
@@ -8,17 +8,20 @@ const {
   getOwner,
 } = Ember;
 
+const tinymceAssetMap = window.__tinymce_addon_asset_map || {};
+let tinymcePromise = null;
+
 export default Ember.Component.extend({
   layout,
 
-  options: {},
+  options: null,
   value: '',
   assetsPrefix: '',
 
   didInsertElement() {
     this._loadTinyMCEAsync().then(() => {
       // tinymce can't handle EmptyObject, so convert to Object by JSON marshalling
-      let options = JSON.parse(JSON.stringify(get(this, 'options')));
+      let options = JSON.parse(JSON.stringify(get(this, 'options') || {}));
 
       options.setup = (editor) => {
         editor.on('change', () => {
@@ -26,20 +29,35 @@ export default Ember.Component.extend({
         });
       };
 
-      options.selector = `#${this.elementId}`;
-
-      window.tinymce.init(options);
+      options.target = this.$('textarea')[0];
+      Ember.run.scheduleOnce('afterRender', this, function() {
+        console.log('INIT', options, $(`#${this.elementId} textarea`));
+        console.log(window.tinymce.init(options));
+      });
     });
   },
 
   _loadTinyMCEAsync() {
     if (typeof window.tinymce !== 'undefined') {
       return new Promise((resolve, reject) => { resolve(); });
-    } else {
+    } /*else if(tinymcePromise !== null) {
+      return tinymcePromise;
+    } */else {
       const assetsPrefix = get(this, 'assetsPrefix');
-      return $.getScript(`${assetsPrefix}assets/tinymce.min.js`).then(() => {
+      tinymcePromise = $.getScript(`${assetsPrefix}assets/tinymce.js`).then(() => {
         window.tinymce.baseURL = `${assetsPrefix}assets`;
+        let oldLoadMethod = tinymce.ScriptLoader.loadScripts;
+        tinymce.ScriptLoader.loadScripts = function(scripts, a, b, c) {
+          scripts = scripts.map(url => {
+            url = url.replace(assetsPrefix, '');
+            // console.log(url, tinymceAssetMap[url] || url);
+            return `${assetsPrefix}${tinymceAssetMap[url] || url}`;
+          });
+
+          return oldLoadMethod(scripts, a, b, c);
+        };
       });
+      return tinymcePromise;
     }
   },
 });
--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 
 const funnel = require('broccoli-funnel');
 const mergeTrees = require('broccoli-merge-trees');
+const resolve = require('resolve');
+const dirname = require('path').dirname;
+const fs = require('fs');
+const glob = require('glob');
 
 module.exports = {
   name: 'ember-tinymce',
@@ -27,14 +31,73 @@ module.exports = {
       trees.push(publicTree);
     }
 
+    let tinymceDir = dirname(resolve.sync('tinymce'));
+
     trees.push(
-      funnel('node_modules/tinymce', {
+      funnel(tinymceDir, {
         destDir: '/assets',
-        exclude: ['*.txt', '*.md']
+        exclude: [
+          '*.txt',
+          '*.md',
+          '**/*.min.js',
+          'tinymce.jquery.js',
+          'jquery.tinymce.js'
+        ]
       })
     );
 
     return mergeTrees(trees);
   },
 
+  postBuild(build) {
+    this._super.included.apply(this, arguments);
+
+    let fingerprintPrepend = '/';
+    let indexFilePath = build.directory + '/index.html';
+    let testIndexFilePath = build.directory + '/tests/index.html';
+
+    let indexFile = fs.readFileSync(indexFilePath, {encoding: 'utf-8'});
+
+    let testIndexFile;
+    if (fs.existsSync(testIndexFilePath)) {
+      testIndexFile = fs.readFileSync(testIndexFilePath, {encoding: 'utf-8'});
+    }
+
+    let files = glob.sync(build.directory + '/assets/**/*', { follow: true });
+    files = files.filter(e => {
+      return fs.statSync(e).isFile();
+    });
+
+    files = files.map(e => {
+      return e.replace(build.directory + '/', '');
+    });
+
+    let totalFiles = files.length;
+
+    let assetMap = {};
+
+    let assetFileName = null;
+    for (let i = 0; i < totalFiles; i++) {
+      let m;
+      if (m = files[i].match(/(.*)-([0-9a-f]+)\.(.*)/i)) {
+        assetMap[`${m[1]}.${m[3]}`] = files[i];
+      }
+    }
+
+    let assetMapPlaceholder = JSON.stringify(assetMap);
+
+
+    fs.writeFileSync(indexFilePath, indexFile.replace(/__ember_tinymce_placeholder__/, assetMapPlaceholder));
+
+    if (testIndexFile) {
+      fs.writeFileSync(testIndexFilePath, testIndexFile.replace(/__ember_tinymce_placeholder__/, assetMapPlaceholder));
+    }
+  },
+
+  contentFor(type, config) {
+    if (type === 'head-footer') {
+      return '<script>var __tinymce_addon_asset_map = __ember_tinymce_placeholder__;</script>';
+    }
+    return '';
+  }
 };
